1a3b4ac22cdaedeb9e29b176bfe768f520d21aa5
Emiliano Martinez <emilianomartinez1933@gmail.com>
2022-09-14 00:56:33 -0300

    class

--- index.js
+++ index.js
@@ -1,5 +1,53 @@
-const Usuario = {
-    nombre: 'emiliano',
-    apellido: 'martinez',
-    mascotas: perros["lola", "pipi"]
+class persona {
+
+    constructor(nombre, apellido, mascotas = []) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.mascotas = mascotas;
+    }
+
+    getFullName() {
+      return (`Hola a todos , soy ${this.nombre} ${this.apellido} , un gusto!`);
+    }
+
+    addPets(nombre) {
+        this.mascotas.push(nombre)
+     }
+
+    countPets() {
+      return `${this.nombre} tiene ${this.mascotas.length} mascotas`
+    }
 }
+
+const perris = [{
+    nombre:'pipi',
+    raza:'galgo'
+},
+{
+    nombre:'lola',
+    raza:'mestizo'
+},
+{
+    nombre:'kevin',
+    raza:'felino'
+}]
+
+const usuario = new persona("emiliano", "martinez");
+
+const addPet = new persona(perris);
+
+addPet.addPets();
+usuario.addPets("pipi");
+usuario.addPets("lola");
+usuario.addPets("kevin")
+usuario.countPets()
+
+usuario.getFullName()
+console.log(usuario.getFullName());
+console.log(usuario.countPets());
+console.log(addPet);
+
+
+
+
+
